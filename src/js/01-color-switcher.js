a4131d037ae6a6bcc3e302454a425b4382191597
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2022-11-20 15:00:16 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
 
+
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.body;
+const delay = 1000;
+let timerId = null;
+
+
+startBtn.addEventListener('click', regularyChangeBg);
+stopBtn.addEventListener('click', stopChangeBg);
+stopBtn.disabled = true;
+
+
+function regularyChangeBg() {
+    timerId = setInterval(changeBgC, delay);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+function stopChangeBg() {
+    clearInterval(timerId);
+    stopBtn.disabled = true;
+    startBtn.disabled = false;
+};
+
+function changeBgC() {
+    body.style.background = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
