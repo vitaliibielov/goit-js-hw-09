a5852867cc1800dc49885cbdae7a0973c1a1f7d4
Vitalii Bielov <vitaliybelov.bvs@gmail.com>
2022-11-20 19:16:14 +0200

    3rd task done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from "notiflix";
+
+
+const form = document.querySelector(".form");
+const { amount, delay, step } = form.elements;
+
+
+form.addEventListener("submit", onFormSubmit);
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  let delayValue = +delay.value;
+  const amountValue = +amount.value;
+  const stepValue = +step.value;
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayValue += stepValue;
   }
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+          // Fulfill
+          resolve({ position, delay });
+      } else {
+          // Reject
+          reject({ position, delay });
+        }
+    }, delay);
+  });
 }
